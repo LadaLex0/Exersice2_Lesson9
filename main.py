234b7d844e42254a6e4cd9fbc6f45e4612fdc2e7
LadaLex0 <91010364+LadaLex0@users.noreply.github.com>
2021-10-18 10:23:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+def koncate(letter, x, y ):  return letter+str(x+y)
+list=str(input('Write the string: '))
+n1=int(input('Write the first number:'))
+n2=int(input('Write the second number:'))
+print(koncate(list, n1, n2))
